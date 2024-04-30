20215648cddd9a16753801f944cf382584863a04
Nojus <78691849+NoahIT@users.noreply.github.com>
2024-04-30 13:17:45 +0000

    Initial commit

---
+++ DBP/DBP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DBP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
