4b58a068871a402d6c2c1c848733eae28a378c11
stylew8 <vaniasavochkin@gmail.com>
2024-05-01 16:11:45 +0300

    deleted dotnet 8

--- DBP/DBP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DBP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
