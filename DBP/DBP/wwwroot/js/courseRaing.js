25f8f2cbd96f76517a318c3be944907d7317822c
Nojus <78691849+NoahIT@users.noreply.github.com>
2024-05-08 22:31:55 +0000

    fixes

--- DBP/DBP/wwwroot/js/courseRaing.js
+++ DBP/DBP/wwwroot/js/courseRaing.js
@@ -1,5 +1,11 @@
-﻿document.querySelectorAll('.star-rating input').forEach(item => {
-    item.addEventListener('change', () => {
-        document.getElementById('ratingForm').submit();
+﻿//document.querySelectorAll('.star-rating input').forEach(item => {
+//    item.addEventListener('change', () => {
+//        document.getElementById('ratingForm').submit();
+//    });
+//});
+
+document.querySelectorAll('.star-rating input').forEach(item => {
+    item.addEventListener('change', event => {
+        alert('You rated this course ' + event.target.value + ' out of 5');
     });
 });
